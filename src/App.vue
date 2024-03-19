855de8735938a20566b22b6662cf8c48d615ffd5
Md All Faysal <all.faysal@gmail.com>
2024-03-19 23:40:25 +0600

    event practice

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+const counter = ref(0);
+
+const counterHandler = (arg, event) => {
+  console.log(event ,arg, "i am called");
+}
+
+const outer = () => {
+  console.log("outer");
+}
+
+const middler = () => {
+  console.log("middler");
+}
+
+const inner = () => {
+  console.log("inner");
+}
+
+const eventHandler = (event) => {
+  console.log(event);
+  console.log("i am called from event");
+}
+
 </script>
 
 <template>
   <h2 class="text-red-600">Successfully Install Tailwind</h2>
+  <button @click="counter++" class="p-4 bg-slate-50 text-blue-300">{{ counter }}</button>
+  <button @click.once="counterHandler('from argument', $event)" class="p-4 bg-slate-50 text-blue-300">'once' {{ counter }}</button>
+  
+  <button @click="eventHandler" class="p-4 bg-yellow-800 text-blue-300">{{ counter }}</button>
+
+  <!-- capturing -->
+  <div id="outer" @click="outer" class="p-8 bg-red-300 my-8">
+    <!-- stop propagation from here -->
+    <div id="middler" @click.stop="middler" class="p-8 bg-blue-400"> 
+      <div id="innser" @click="inner" class="p-8 bg-green-400">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
